15631220127649283987d0283f516f9ec7cfd4ce
Konstantin Grigorev <info@a6raywa1cher.com>
2022-02-23 01:00:02 +0300

    Fix JPA incompatibility of IUserRepository

--- src/main/java/com/a6raywa1cher/jsonrestsecurity/dao/repo/IUserRepository.java
+++ src/main/java/com/a6raywa1cher/jsonrestsecurity/dao/repo/IUserRepository.java
@@ -1,18 +1,17 @@
 package com.a6raywa1cher.jsonrestsecurity.dao.repo;
 
 import com.a6raywa1cher.jsonrestsecurity.dao.model.IUser;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 /**
- * Provides methods to operate User objects
- * <br/>
- * Complies with Spring Data JPA {@link org.springframework.data.jpa.repository.JpaRepository} notation
+ * Provides methods to operate User objects. Extends the {@link CrudRepository} interface.
  *
  * @param <T> exact implementation of {@link IUser} interface
  * @see DefaultUserRepository
  */
-public interface IUserRepository<T extends IUser> {
+public interface IUserRepository<T extends IUser> extends CrudRepository<T, Long> {
 	Optional<T> findByUsername(String username);
 
 	<S extends T> S save(S s);
